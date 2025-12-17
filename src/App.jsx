eef34b87f957c96ad48ff76a2e2f8487c4ca806c
lahceenn <lahcenouhassou18@gmail.com>
2025-12-17 19:52:06 +0100

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center text-4xl font-bold">
-      Marketing Agency 🚀
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
