b6b3e12f579d3a3dc207c596fce47ef19b1e1831
lahceenn <lahcenouhassou18@gmail.com>
2025-12-26 17:42:12 +0100

    Upd

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
+import MainLayout from "./components/layout/MainLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route element={<MainLayout />}>
+          {/* ✅ الصفحة الرئيسية */}
+          <Route index element={<Home />} />
+
+          {/* باقي الصفحات */}
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
