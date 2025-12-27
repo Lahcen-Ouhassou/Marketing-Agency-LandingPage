23b047d6017e8127275bd012261403b96a554fc8
lahceenn <lahcenouhassou18@gmail.com>
2025-12-27 19:12:36 +0100

    Upd

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="p-4 border-b flex gap-6">
+    <nav className="p-7 border-b flex gap-6">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/services">Services</Link>
