fcdf0a2d12f0c5b7898859a34b677844336633f9
lahceenn <lahcenouhassou18@gmail.com>
2025-12-29 18:34:39 +0100

    NewNavBar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="p-7 border-b flex gap-6">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/services">Services</Link>
-      <Link to="/contact">Contact</Link>
-    </nav>
+    <header className="border-b">
+      <div className="max-w-7xl mx-auto px-4 h-16 flex justify-between items-center">
+        <span className="font-bold text-xl">MarketingPro</span>
+
+        <nav className="flex gap-6">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </div>
+    </header>
   );
 }
 
