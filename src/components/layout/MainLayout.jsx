2cc1451dc8bee19a87e6c20a26e7fbbcfa0e59f5
lahceenn <lahcenouhassou18@gmail.com>
2025-12-29 18:35:27 +0100

    NewMainLayout

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,14 +1,18 @@
-import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Outlet />
+
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
 
