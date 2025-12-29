ff653dc3f468ac2ec25e2c597b9bbf144f071970
lahceenn <lahcenouhassou18@gmail.com>
2025-12-30 00:07:14 +0100

    UPDATEEE

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-950 text-white">
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-        </Route>
-      </Routes>
-    </div>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 }
 
