d658449da70f6da07ef95a3f5daf20e1df3afad2
lahceenn <lahcenouhassou18@gmail.com>
2026-01-03 18:24:30 +0100

    UpdNavbarPages

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,12 +5,12 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="border-b">
-      <div className="mx-auto max-w-screen-xl px-4 py-4 flex items-center justify-between">
-        <h1 className="text-xl font-bold">Marketing Agency</h1>
+    <header className="">
+      <div className="mx-auto max-w-screen-xl px-4 py-5 flex items-center justify-between">
+        <h1 className="text-xl font-bold text-white">Marketing Agency</h1>
 
         {/* Desktop */}
-        <nav className="hidden md:flex gap-6 text-sm">
+        <nav className="hidden md:flex gap-6 text-l text-white">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/services">Services</Link>
@@ -19,7 +19,7 @@ export default function Navbar() {
 
         {/* Mobile button */}
         <button
-          className="md:hidden cursor-pointer "
+          className="md:hidden cursor-pointer text-white"
           onClick={() => setOpen(!open)}
         >
           ☰☰
@@ -29,7 +29,7 @@ export default function Navbar() {
       {/* Mobile menu */}
 
       {open && (
-        <div className="md:hidden grid grid-cols-1 border-t px-4 py-4 space-y-3">
+        <div className="md:hidden grid grid-cols-1  px-4 py-4 space-y-3 text-white cursor-pointer">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/services">Services</Link>
