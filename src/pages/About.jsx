4eecbbfc99df208731357e22e33ecdf487f1bee8
lahceenn <lahcenouhassou18@gmail.com>
2026-01-04 01:35:57 +0100

    UpdAboutPage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,15 @@
 function About() {
   return (
-    <section className="py-20 text-center text-white">
+    <section className="py-20 text-center  bg-white">
       <h1 className="text-4xl font-bold mb-4">About Us</h1>
+      <p className="text-muted-foreground">We are a modern marketing agency.</p>
+       <h1 className="text-4xl font-bold mb-4">About Us</h1>
+      <p className="text-muted-foreground">We are a modern marketing agency.</p>
+       <h1 className="text-4xl font-bold mb-4">About Us</h1>
+      <p className="text-muted-foreground">We are a modern marketing agency.</p>
+       <h1 className="text-4xl font-bold mb-4">About Us</h1>
+      <p className="text-muted-foreground">We are a modern marketing agency.</p>
+       <h1 className="text-4xl font-bold mb-4">About Us</h1>
       <p className="text-muted-foreground">We are a modern marketing agency.</p>
     </section>
   );
