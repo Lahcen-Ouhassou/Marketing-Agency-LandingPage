c33d206bd50eb1aa92f06109f83ccce12146435e
lahceenn <lahcenouhassou18@gmail.com>
2026-01-05 14:46:27 +0100

    UpdFraturesPages

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -4,9 +4,7 @@ function Features() {
       <div className="mx-auto max-w-7xl px-4">
         {/* Title */}
         <div className="mb-16 text-center">
-          <h2 className="text-3xl font-bold sm:text-4xl">
-            Our Features
-          </h2>
+          <h2 className="text-3xl font-bold sm:text-4xl">Our Features</h2>
           <p className="mt-4 text-gray-600">
             Everything you need to grow your business online
           </p>
@@ -15,38 +13,233 @@ function Features() {
         {/* Cards */}
         <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {/* Card 1 */}
-          <div className="rounded-xl bg-white p-8 shadow-sm">
-            <h3 className="text-xl font-semibold">
-              Fast Performance
+          <a
+            href="#"
+            class="block border-2 border-black bg-white p-4 shadow-[4px_4px_0_0,8px_8px_0_0,12px_12px_0_0] hover:translate-3 hover:bg-yellow-100 hover:shadow-none focus:ring-2 focus:ring-yellow-300 focus:outline-0 sm:p-6"
+          >
+            <span class="inline-flex items-center gap-1.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                class="size-4"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4 1.75a.75.75 0 0 1 1.5 0V3h5V1.75a.75.75 0 0 1 1.5 0V3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2V1.75ZM4.5 6a1 1 0 0 0-1 1v4.5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-7Z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+
+              <time
+                datetime="2025-04-01"
+                class="text-xs/none font-semibold uppercase"
+              >
+                April 1, 2025
+              </time>
+            </span>
+
+            <h3 class="mt-1 text-xl font-semibold">
+              How I built my first website with Nuxt, Tailwind CSS and Vercel
             </h3>
-            <p className="mt-3 text-gray-600">
-              Optimized solutions for fast and smooth user experience.
+
+            <p class="mt-2 line-clamp-2 text-pretty">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit
+              illum provident a, ipsa maiores deleniti consectetur nobis et
+              eaque.
             </p>
-          </div>
+          </a>
 
           {/* Card 2 */}
-          <div className="rounded-xl bg-white p-8 shadow-sm">
-            <h3 className="text-xl font-semibold">
-              Modern Design
-            </h3>
-            <p className="mt-3 text-gray-600">
-              Clean and modern UI that converts visitors into clients.
-            </p>
-          </div>
+          <a
+            href="#"
+            class="block border-2 border-black bg-white p-4 shadow-[4px_4px_0_0,8px_8px_0_0,12px_12px_0_0] hover:translate-3 hover:bg-yellow-100 hover:shadow-none focus:ring-2 focus:ring-yellow-300 focus:outline-0 sm:p-6"
+          >
+            <span class="inline-flex items-center gap-1.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                class="size-4"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4 1.75a.75.75 0 0 1 1.5 0V3h5V1.75a.75.75 0 0 1 1.5 0V3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2V1.75ZM4.5 6a1 1 0 0 0-1 1v4.5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-7Z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
 
-          {/* Card 3 */}
-          <div className="rounded-xl bg-white p-8 shadow-sm">
-            <h3 className="text-xl font-semibold">
-              SEO Optimized
+              <time
+                datetime="2025-04-01"
+                class="text-xs/none font-semibold uppercase"
+              >
+                April 1, 2025
+              </time>
+            </span>
+
+            <h3 class="mt-1 text-xl font-semibold">
+              How I built my first website with Nuxt, Tailwind CSS and Vercel
             </h3>
-            <p className="mt-3 text-gray-600">
-              Improve your visibility on search engines easily.
+
+            <p class="mt-2 line-clamp-2 text-pretty">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit
+              illum provident a, ipsa maiores deleniti consectetur nobis et
+              eaque.
             </p>
-          </div>
+          </a>
+          {/* Card 3 */}
+          <a
+            href="#"
+            class="block border-2 border-black bg-white p-4 shadow-[4px_4px_0_0,8px_8px_0_0,12px_12px_0_0] hover:translate-3 hover:bg-yellow-100 hover:shadow-none focus:ring-2 focus:ring-yellow-300 focus:outline-0 sm:p-6"
+          >
+            <span class="inline-flex items-center gap-1.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                class="size-4"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4 1.75a.75.75 0 0 1 1.5 0V3h5V1.75a.75.75 0 0 1 1.5 0V3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2V1.75ZM4.5 6a1 1 0 0 0-1 1v4.5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-7Z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+
+              <time
+                datetime="2025-04-01"
+                class="text-xs/none font-semibold uppercase"
+              >
+                April 1, 2025
+              </time>
+            </span>
+
+            <h3 class="mt-1 text-xl font-semibold">
+              How I built my first website with Nuxt, Tailwind CSS and Vercel
+            </h3>
+
+            <p class="mt-2 line-clamp-2 text-pretty">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit
+              illum provident a, ipsa maiores deleniti consectetur nobis et
+              eaque.
+            </p>
+          </a>
+
+          {/* Card 4 */}
+          <a
+            href="#"
+            class="block border-2 border-black bg-white p-4 shadow-[4px_4px_0_0,8px_8px_0_0,12px_12px_0_0] hover:translate-3 hover:bg-yellow-100 hover:shadow-none focus:ring-2 focus:ring-yellow-300 focus:outline-0 sm:p-6"
+          >
+            <span class="inline-flex items-center gap-1.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                class="size-4"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4 1.75a.75.75 0 0 1 1.5 0V3h5V1.75a.75.75 0 0 1 1.5 0V3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2V1.75ZM4.5 6a1 1 0 0 0-1 1v4.5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-7Z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+
+              <time
+                datetime="2025-04-01"
+                class="text-xs/none font-semibold uppercase"
+              >
+                April 1, 2025
+              </time>
+            </span>
+
+            <h3 class="mt-1 text-xl font-semibold">
+              How I built my first website with Nuxt, Tailwind CSS and Vercel
+            </h3>
+
+            <p class="mt-2 line-clamp-2 text-pretty">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit
+              illum provident a, ipsa maiores deleniti consectetur nobis et
+              eaque.
+            </p>
+          </a>
+          {/* Card 5 */}
+          <a
+            href="#"
+            class="block border-2 border-black bg-white p-4 shadow-[4px_4px_0_0,8px_8px_0_0,12px_12px_0_0] hover:translate-3 hover:bg-yellow-100 hover:shadow-none focus:ring-2 focus:ring-yellow-300 focus:outline-0 sm:p-6"
+          >
+            <span class="inline-flex items-center gap-1.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                class="size-4"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4 1.75a.75.75 0 0 1 1.5 0V3h5V1.75a.75.75 0 0 1 1.5 0V3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2V1.75ZM4.5 6a1 1 0 0 0-1 1v4.5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-7Z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+
+              <time
+                datetime="2025-04-01"
+                class="text-xs/none font-semibold uppercase"
+              >
+                April 1, 2025
+              </time>
+            </span>
+
+            <h3 class="mt-1 text-xl font-semibold">
+              How I built my first website with Nuxt, Tailwind CSS and Vercel
+            </h3>
+
+            <p class="mt-2 line-clamp-2 text-pretty">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit
+              illum provident a, ipsa maiores deleniti consectetur nobis et
+              eaque.
+            </p>
+          </a>
+          {/* Card 2 */}
+          <a
+            href="#"
+            class="block border-2 border-black bg-white p-4 shadow-[4px_4px_0_0,8px_8px_0_0,12px_12px_0_0] hover:translate-3 hover:bg-yellow-100 hover:shadow-none focus:ring-2 focus:ring-yellow-300 focus:outline-0 sm:p-6"
+          >
+            <span class="inline-flex items-center gap-1.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                class="size-4"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4 1.75a.75.75 0 0 1 1.5 0V3h5V1.75a.75.75 0 0 1 1.5 0V3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2V1.75ZM4.5 6a1 1 0 0 0-1 1v4.5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-7Z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+
+              <time
+                datetime="2025-04-01"
+                class="text-xs/none font-semibold uppercase"
+              >
+                April 1, 2025
+              </time>
+            </span>
+
+            <h3 class="mt-1 text-xl font-semibold">
+              How I built my first website with Nuxt, Tailwind CSS and Vercel
+            </h3>
+
+            <p class="mt-2 line-clamp-2 text-pretty">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit
+              illum provident a, ipsa maiores deleniti consectetur nobis et
+              eaque.
+            </p>
+          </a>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Features
+export default Features;
