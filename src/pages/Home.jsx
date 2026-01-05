005bf35a7fdc33f0cf2648349c906ae9aa1799e2
lahceenn <lahcenouhassou18@gmail.com>
2026-01-05 14:34:09 +0100

    Upd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import Hero from "@/components/sections/Hero";
-import Features from "@/components/sections/Features";
-import FAQ from "@/components/sections/FAQ";
+import Hero from "../components/sections/Hero"
+import Features from "../components/sections/Features"
+import FAQ from "../components/sections/FAQ"
 
 function Home() {
   return (
