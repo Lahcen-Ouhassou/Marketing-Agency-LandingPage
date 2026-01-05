5ede0375b016fb71629ed590f3e61e94d8d82144
lahceenn <lahcenouhassou18@gmail.com>
2026-01-05 17:09:18 +0100

    UpdNavbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -17,8 +17,8 @@ export default function Navbar() {
         {/* Desktop */}
         <nav className="hidden md:flex gap-6 text-l text-white">
           <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
           <Link to="/services">Services</Link>
+          <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
         </nav>
 
