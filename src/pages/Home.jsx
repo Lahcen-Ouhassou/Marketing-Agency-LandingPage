4211d5ba8c1a2b54e3225581240713cdd8508eb0
lahceenn <lahcenouhassou18@gmail.com>
2026-01-06 15:30:04 +0100

    UpdHome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/sections/Hero"
 import Features from "../components/sections/Features"
 import FAQ from "../components/sections/FAQ"
+import WhyChooseUs from "../components/sections/WhyChooseUs"
 
 function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <WhyChooseUs />
       <FAQ />
     </>
   );
