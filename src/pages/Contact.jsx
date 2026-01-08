8b0911bd619b9dbe24473a45d104444fa45c4158
lahceenn <lahcenouhassou18@gmail.com>
2026-01-08 14:16:04 +0100

    UpdContact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -18,9 +18,9 @@ const Contact = () => {
           {/* ----- Info ----- */}
           <div className="space-y-8">
             {/* Address */}
-            <div className="flex items-start gap-4">
+            <div className="flex items-start gap-3">
               <svg
-                className="shrink-0 w-6 h-6 text-neutral-500"
+                className="shrink-0 w-5 h-6 text-neutral-500"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -34,15 +34,15 @@ const Contact = () => {
                 <h4 className="font-semibold text-white">Address</h4>
                 <p className="text-gray-400 text-sm">
                   123 Marketing St.
-                  <br /> New York, NY 10001
+                  <br /> Agadir, MR 80000
                 </p>
               </div>
             </div>
 
             {/* Email */}
-            <div className="flex items-start gap-4">
+            <div className="flex items-start gap-3">
               <svg
-                className="shrink-0 w-6 h-6 text-neutral-500"
+                className="shrink-0 w-5 h-6 text-neutral-500"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -59,17 +59,22 @@ const Contact = () => {
             </div>
 
             {/* Phone */}
-            <div className="flex items-start gap-4">
-              <svg
-                className="shrink-0 w-6 h-6 text-neutral-500"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={1.5}
-              >
-                <path d="M3 10a1 1 0 0 1 1-1h2.5a1 1 0 0 1 1 .7l.6 2a1 1 0 0 1-.2 1l-1.2 1.2a16 16 0 0 0 6 6l1.2-1.2a1 1 0 0 1 1-.2l2 .6a1 1 0 0 1 .7 1V21a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-11Z" />
-              </svg>
+            <div className="flex items-start gap-3">
+            <svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth={1.5}
+  stroke="currentColor"
+  className="shrink-0 w-5 h-6 text-neutral-500"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+  />
+</svg>
+
               <div>
                 <h4 className="font-semibold text-white">Phone</h4>
                 <p className="text-gray-400 text-sm">+1 234 567 890</p>
@@ -77,9 +82,9 @@ const Contact = () => {
             </div>
 
             {/* Hiring */}
-            <div className="flex items-start gap-4">
+            <div className="flex items-start gap-3">
               <svg
-                className="shrink-0 w-6 h-6 text-neutral-500"
+                className="shrink-0 w-5 h-6 text-neutral-500"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -122,7 +127,7 @@ const Contact = () => {
               className="w-full max-h-48 min-h-25 p-4 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-400 "
               style={{ backgroundColor: "#0B1020" }}
             ></textarea>
-            <button className="mt-4 px-6 py-2 bg-indigo-700 text-neutral-900 rounded-r-3xl font-medium hover:bg-indigo-400 transition">
+            <button className="mt-4 px-6 py-2 bg-indigo-700 text-neutral-900 rounded-r-3xl font-medium hover:bg-indigo-500 transition">
               Submit
             </button>
           </div>
