0386625dd0b0be9fd60562dd8a8c3face3876a93
lahceenn <lahcenouhassou18@gmail.com>
2026-01-09 17:38:58 +0100

    UpdAboutPage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,5 @@
+import TextType from "@/components/TextType";
+
 function About() {
   return (
     <section className="py-24 bg-gradient-to-br from-slate-900 via-slate-800 to-indigo-900 text-gray-100 h-217">
@@ -14,20 +16,20 @@ function About() {
 
         {/* ===== CONTENT ===== */}
         <div className="max-w-4xl">
+         <TextType
+  text={`We are a digital marketing agency dedicated to helping businesses grow and succeed in the online world. Our team combines creativity, strategy, and data-driven insights to deliver impactful marketing solutions.
+
+From building strong brand identities to managing high-performing campaigns, we focus on measurable results and long-term success. We believe in transparency, innovation, and creating real value for our clients through modern digital strategies.`}
+    typingSpeed={20}
+  pauseDuration={1000}
+  deletingSpeed={20}
+  cursorBlinkDuration={0.9}
+  loop={false}
+  className="text-lg leading-relaxed text-gray-300 break-words"
+ />
+
           
-          <p className="text-lg leading-relaxed text-gray-300">
-            
-            We are a digital marketing agency dedicated to helping businesses
-            grow and succeed in the online world. Our team combines creativity,
-            strategy, and data-driven insights to deliver impactful marketing
-            solutions.
-            <br />
-            <br />
-            From building strong brand identities to managing high-performing
-            campaigns, we focus on measurable results and long-term success. We
-            believe in transparency, innovation, and creating real value for our
-            clients through modern digital strategies.
-          </p>
+        
         </div>
       </div>
     </section>
