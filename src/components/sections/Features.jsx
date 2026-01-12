de3bb7d60f296e0de1abbe474a5709bd834360d2
lahceenn <lahcenouhassou18@gmail.com>
2026-01-12 19:49:24 +0100

    UpdSectionFeatures

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -1,16 +1,31 @@
+import ScrollFloat from '../ScrollFloat';
+import AnimatedContent from '../AnimatedContent';
+
 function Features() {
   return (
     <section id="our-features" className="bg-gray-50 py-24">
       <div className="mx-auto max-w-7xl px-4">
         {/* Title */}
         <div className="mb-16 text-center">
-          <h2 className="text-3xl font-bold sm:text-4xl">Our Features</h2>
+          <ScrollFloat
+  animationDuration={1}
+  ease="back.inOut(2)"
+  scrollStart="center bottom+=50%"
+  scrollEnd="bottom bottom-=40%"
+  stagger={0.03}
+  containerClassName=""
+  textClassName="text-4xl font-bold sm:text-4xl"
+>
+  Our Features 
+</ScrollFloat>
+
           <p className="mt-4 text-gray-600">
             Everything you need to grow your business online
           </p>
         </div>
 
         {/* Cards */}
+         <AnimatedContent direction="vertical" reverse={false} disappearEase="bounce.in">
         <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {/* Card 1 */}
           <a class="block border-2 border-black bg-white p-4 shadow-[4px_4px_0_0,8px_8px_0_0] hover:translate-3 hover:bg-indigo-400 hover:text-white hover:shadow-none focus:ring-2 focus:ring-yellow-300 focus:outline-0 sm:p-6">
@@ -71,7 +86,9 @@ function Features() {
             </p>
           </a>
         </div>
+        </AnimatedContent>
       </div>
+      
     </section>
   );
 }
